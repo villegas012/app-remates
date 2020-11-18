9e45418a5c638fbb15b9c67df8274e6cb722eb3b
Fidel Villegas <fidel.villegaz@gmail.com>
2020-11-18 15:31:11 -0500

    Se realiza Route y componente producto

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -1,39 +1,58 @@
 import React from 'react';
-import { Container, Button, Col, Row, ButtonGroup, Dropdown } from 'react-bootstrap';
+import {Button, Col, Row, ButtonGroup, Dropdown } from 'react-bootstrap';
 import SearchBar from '../searchBar/SearchBar.jsx'
 import fondo from '../../imagenes/fondo.jpg'
+import { useHistory } from 'react-router-dom';
 
 
 
 export default function Home() {
+
+    const style1 = { display: 'flex', justifyContent: 'center'};
+    const history = useHistory()
+
+
+    const handleClick = (e) => {
+       e.preventDefault();
+        if(e.target.value === 'Productos'){
+            history.push(`/productos`)
+        }
+
+    }
+
     return (
-        <div>            
-            <Row className='headHome' >            
-                <Col> 
-                <h1 style={{margin: '50px', fontSize:'80px'}}>Remapp</h1>               
-                <ButtonGroup aria-label="Basic example">
-                    <Button>Productos</Button>
-                    <Dropdown.Item>Categorias</Dropdown.Item>
-                    <Button>Ingresa</Button>
-                    <Button>Registrate</Button>
-                    
-                </ButtonGroup>
+        <div>
+            <Row className='headHome' >
+                <Col style={style1}>
+                    <div>
+                        <h1 style={{ margin: '50px', fontSize: '80px' }}>Remapp</h1>
+                        <ButtonGroup aria-label="Basic example">
+                            <Button value='Productos' onClick={handleClick}>Productos</Button>
+                            <Dropdown.Item>Categorias</Dropdown.Item>
+                            <Button value='Ingresa'>Ingresa</Button>
+                            <Button value='Registrate'>Registrate</Button>
+                        </ButtonGroup>
+                    </div>
                 </Col>
             </Row>
-            <Container className='bodyHome' style={{ marginTop: '50px' }}>
-                <div style={{display: "flex", justifyContent:'center'}}>
-                    <SearchBar/>    
-                </div>
-                <div style={{marginTop: '20px'}}>
-                    <img src={fondo} style={{width:'60%'}}></img>
-                </div>
-            </Container>
-            <div className='botonesHome'>
-
-            </div>
-            <div className='footerHome'>
-
-            </div>
+            <Row className='bodyHome'>
+                <Col style={style1}>
+                    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+                        <SearchBar />
+                    </div>
+                </Col>  
+            </Row>
+            <Row>
+            <Col style={style1}>
+                    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+                        <img src={fondo} style={{width:'50%'}}></img>
+                    </div>
+                </Col>
+            </Row>
         </div>
+
+
+
     )
 }
+
